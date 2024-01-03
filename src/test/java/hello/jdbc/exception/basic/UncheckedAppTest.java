a7edec8e509990ea1d0ffbcd1fd26cc351f3d692
a2175 <as217512@gmail.com>
2024-01-03 18:38:21 +0900

    예외 전환 - 예외 포함과 스택 트레이스

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -18,6 +18,18 @@ public class UncheckedAppTest {
                 .isInstanceOf(RuntimeException.class);
     }
 
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+
+        try {
+            controller.request();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            log.info("ex", e);
+        }
+    }
+
     static class Controller {
 
         Service service = new Service();
